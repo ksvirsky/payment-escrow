4118152cc802546ca3e0cc36798ebc45a6dfada6
Kos <k.svirski@gmail.com>
2025-05-27 23:32:33 +0200

    - added amoy config.;

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
+
+const privateKey = process.env.DEPLOYER_KEY?.trim() || "0x0";
 
 const config: HardhatUserConfig = {
+  networks: {
+    amoy: {
+      url: "https://rpc-amoy.polygon.technology/",
+      chainId: 80002,
+      gasPrice: 10000000,
+      blockGasLimit: 3000000,
+      gas: 3000000,
+      accounts: [privateKey],
+    },
+  },
+
   solidity: "0.8.30",
 };
 
